c84be1ddbe31f001873b88e1c7318e5e1ed0ec03
seeeek-debug <seeeekaaaai.0126@gmail.com>
2026-09-13 01:33:27 +0900

    Update backtest_0912.py

--- backtest_0912.py
+++ backtest_0912.py
@@ -6,7 +6,7 @@ import itertools
 
 # --- 基本設定 ---
 MODEL_FILENAME = "boatrace_lgb_model.pkl"
-TARGET_DATE = "2026-09-08"
+TARGET_DATE = "2026-09-07"
 
 VENUES = [
     "桐生", "戸田", "江戸川", "平和島", "多摩川", "浜名湖",
